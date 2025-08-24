fbd27fb8ac8ea5706ced2ef81237bbb6168e2b94
jncldrn64 <jncldrn64@users.noreply.github.com>
2025-08-24 02:23:30 -0500

    ADD: Entiti

---
+++ demo/src/main/java/app/domain/entity_models/VitalSigns.java
@@ -0,0 +1,11 @@
+package app.domain.entity_models;
+
+public class VitalSigns {
+    
+    private String patientDocument;
+    private LocalDate recordDate;
+    private String bloodPressure;
+    private double temperature;
+    private int pulse;
+    private double oxygenLevel;
+}
